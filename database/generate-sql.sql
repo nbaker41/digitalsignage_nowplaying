4fa173fae0d910db7fa2f1ce93ac012d9453ba6d
Dunston <cdunston3@gatech.edu>
2020-02-11 08:36:54 -0500

    updating order, content of db tables

--- database/generate-sql.sql
+++ database/generate-sql.sql
@@ -1,106 +1,3 @@
--- Warning: You can generate script only for two tables at a time in your Free plan 
--- 
--- ****************** SqlDBM: MySQL ******************;
--- ***************************************************;
-
-
--- ************************************** `albums`
-
-CREATE TABLE `albums`
-(
- `album_id`    int NOT NULL ,
- `name`        varchar(250) NOT NULL ,
- `description` mediumtext NULL ,
- `created`     datetime NOT NULL ,
- `type`        varchar(250) NOT NULL ,
- `customer_id` int NOT NULL ,
- `user_id`     int NOT NULL ,
-
-PRIMARY KEY (`album_id`),
-KEY `fkIdx_147` (`customer_id`),
-CONSTRAINT `FK_147` FOREIGN KEY `fkIdx_147` (`customer_id`) REFERENCES `customers` (`customer_id`),
-KEY `fkIdx_150` (`user_id`),
-CONSTRAINT `FK_150` FOREIGN KEY `fkIdx_150` (`user_id`) REFERENCES `users` (`user_id`)
-);
-
-
-
-
-
-
--- ************************************** `albums-directories`
-
-CREATE TABLE `albums-directories`
-(
- `album_id`     int NOT NULL ,
- `directory_id` int NOT NULL ,
- `created`      datetime NOT NULL ,
- `user_id`      int NOT NULL ,
- `order`        int NOT NULL ,
-
-PRIMARY KEY (`album_id`, `directory_id`),
-KEY `fkIdx_161` (`album_id`),
-CONSTRAINT `FK_161` FOREIGN KEY `fkIdx_161` (`album_id`) REFERENCES `albums` (`album_id`),
-KEY `fkIdx_165` (`directory_id`),
-CONSTRAINT `FK_165` FOREIGN KEY `fkIdx_165` (`directory_id`) REFERENCES `directories` (`directory_id`),
-KEY `fkIdx_169` (`user_id`),
-CONSTRAINT `FK_169` FOREIGN KEY `fkIdx_169` (`user_id`) REFERENCES `users` (`user_id`)
-);
-
-
-
-
-
--- ************************************** `albums_media`
-
-CREATE TABLE `albums_media`
-(
- `album_id` int NOT NULL ,
- `media_id` int NOT NULL ,
- `user_id`  int NOT NULL ,
- `created`  datetime NOT NULL ,
- `order`    int NOT NULL ,
-
-PRIMARY KEY (`album_id`, `media_id`),
-KEY `fkIdx_198` (`album_id`),
-CONSTRAINT `FK_198` FOREIGN KEY `fkIdx_198` (`album_id`) REFERENCES `albums` (`album_id`),
-KEY `fkIdx_202` (`media_id`),
-CONSTRAINT `FK_202` FOREIGN KEY `fkIdx_202` (`media_id`) REFERENCES `media` (`media_id`),
-KEY `fkIdx_205` (`user_id`),
-CONSTRAINT `FK_205` FOREIGN KEY `fkIdx_205` (`user_id`) REFERENCES `users` (`user_id`)
-);
-
-
-
-
-
--- ****************** SqlDBM: MySQL ******************;
--- ***************************************************;
-
-
--- ************************************** `buildings`
-
-CREATE TABLE `buildings`
-(
- `building_id`    int NOT NULL ,
- `name_short`     varchar(250) NOT NULL ,
- `name_long`      varchar(250) NOT NULL ,
- `geo_latitude`   decimal(16,8) NULL ,
- `geo_longitude`  decimal(16,8) NULL ,
- `address_number` int NULL ,
- `address_street` varchar(250) NULL ,
- `location_city`  varchar(250) NOT NULL ,
- `location_state` varchar(250) NOT NULL ,
- `location_zip`   varchar(250) NOT NULL ,
-
-PRIMARY KEY (`building_id`)
-);
-
-
-
-
--- Warning: You can generate script only for two tables at a time in your Free plan 
--- 
 -- ****************** SqlDBM: MySQL ******************;
 -- ***************************************************;
 
@@ -110,12 +7,35 @@ PRIMARY KEY (`building_id`)
 CREATE TABLE `customers`
 (
  `customer_id` int NOT NULL ,
+ `created`     datetime NOT NULL ,
  `name_short`  varchar(250) NOT NULL ,
  `name_long`   varchar(250) NOT NULL ,
 
 PRIMARY KEY (`customer_id`)
 );
 
+
+
+
+
+
+-- ************************************** `buildings`
+
+CREATE TABLE `buildings`
+(
+ `building_id`    int NOT NULL ,
+ `name_short`     varchar(250) NOT NULL ,
+ `name_long`      varchar(250) NOT NULL ,
+ `location_city`  varchar(250) NOT NULL ,
+ `location_state` varchar(250) NOT NULL ,
+ `location_zip`   varchar(250) NOT NULL ,
+ `address_number` int NULL ,
+ `address_street` varchar(250) NULL ,
+ `geo_latitude`   decimal(16,8) NULL ,
+ `geo_longitude`  decimal(16,8) NULL ,
+
+PRIMARY KEY (`building_id`)
+);
 
 
 
@@ -174,9 +94,10 @@ CREATE TABLE `customers_users`
 (
  `customer_id` int NOT NULL ,
  `user_id`     int NOT NULL ,
- `lastlogin`   datetime NOT NULL ,
- `admin`       bit(1) NOT NULL ,
  `created`     datetime NOT NULL ,
+ `admin`       bit(1) NOT NULL ,
+ `login_last`  datetime NOT NULL ,
+ `login_count` int NOT NULL ,
 
 PRIMARY KEY (`customer_id`, `user_id`),
 KEY `fkIdx_27` (`customer_id`),
@@ -234,12 +155,12 @@ PRIMARY KEY (`department_id`)
 CREATE TABLE `directories`
 (
  `directory_id` int NOT NULL ,
- `name`         varchar(250) NOT NULL ,
- `description`  mediumtext NULL ,
- `contents`     longtext NOT NULL ,
- `created`      datetime NOT NULL ,
  `customer_id`  int NOT NULL ,
  `user_id`      int NOT NULL ,
+ `created`      datetime NOT NULL ,
+ `name`         varchar(250) NOT NULL ,
+ `contents`     longtext NOT NULL ,
+ `description`  mediumtext NULL ,
 
 PRIMARY KEY (`directory_id`),
 KEY `fkIdx_172` (`customer_id`),
@@ -261,14 +182,14 @@ CONSTRAINT `FK_178` FOREIGN KEY `fkIdx_178` (`user_id`) REFERENCES `users` (`use
 CREATE TABLE `media`
 (
  `media_id`          int NOT NULL ,
- `user_id`           int NOT NULL ,
  `customer_id`       int NOT NULL ,
- `name`              varchar(250) NULL ,
- `file_name`         varchar(250) NOT NULL ,
+ `user_id`           int NOT NULL ,
  `created`           datetime NOT NULL ,
- `file_size`         int NOT NULL ,
- `description`       mediumtext NULL ,
+ `file_name`         varchar(250) NOT NULL ,
  `file_type`         varchar(250) NOT NULL ,
+ `file_size`         int NOT NULL ,
+ `name`              varchar(250) NULL ,
+ `description`       mediumtext NULL ,
  `dimensions_height` int NULL ,
  `dimensions_width`  int NULL ,
 
@@ -282,10 +203,6 @@ CONSTRAINT `FK_184` FOREIGN KEY `fkIdx_184` (`customer_id`) REFERENCES `customer
 
 
 
-
-
--- ****************** SqlDBM: MySQL ******************;
--- ***************************************************;
 
 
 -- ************************************** `floors`
@@ -308,8 +225,6 @@ CONSTRAINT `FK_123` FOREIGN KEY `fkIdx_123` (`building_id`) REFERENCES `building
 
 
 
-
-
 -- ****************** SqlDBM: MySQL ******************;
 -- ***************************************************;
 
@@ -319,11 +234,11 @@ CONSTRAINT `FK_123` FOREIGN KEY `fkIdx_123` (`building_id`) REFERENCES `building
 CREATE TABLE `players`
 (
  `player_id`     int NOT NULL ,
- `touch_enabled` bit(1) NOT NULL ,
- `name_short`    varchar(250) NOT NULL ,
- `name_long`     varchar(250) NOT NULL ,
  `customer_id`   int NOT NULL ,
  `floor_id`      int NOT NULL ,
+ `name_short`    varchar(250) NOT NULL ,
+ `name_long`     varchar(250) NOT NULL ,
+ `touch_enabled` bit(1) NOT NULL ,
 
 PRIMARY KEY (`player_id`),
 KEY `fkIdx_108` (`customer_id`),
@@ -337,9 +252,9 @@ CONSTRAINT `FK_120` FOREIGN KEY `fkIdx_120` (`floor_id`) REFERENCES `floors` (`f
 
 
 
--- ************************************** `players_albums`
+-- ************************************** `players_playlists`
 
-CREATE TABLE `players_albums`
+CREATE TABLE `players_playlists`
 (
  `player_id` int NOT NULL ,
  `album_id`  int NOT NULL ,
@@ -351,9 +266,103 @@ PRIMARY KEY (`player_id`, `album_id`),
 KEY `fkIdx_136` (`player_id`),
 CONSTRAINT `FK_136` FOREIGN KEY `fkIdx_136` (`player_id`) REFERENCES `players` (`player_id`),
 KEY `fkIdx_140` (`album_id`),
-CONSTRAINT `FK_140` FOREIGN KEY `fkIdx_140` (`album_id`) REFERENCES `albums` (`album_id`),
+CONSTRAINT `FK_140` FOREIGN KEY `fkIdx_140` (`album_id`) REFERENCES `playlists` (`album_id`),
 KEY `fkIdx_143` (`user_id`),
 CONSTRAINT `FK_143` FOREIGN KEY `fkIdx_143` (`user_id`) REFERENCES `users` (`user_id`)
+);
+
+
+
+
+
+-- ****************** SqlDBM: MySQL ******************;
+-- ***************************************************;
+
+
+-- ************************************** `playlists`
+
+CREATE TABLE `playlists`
+(
+ `album_id`    int NOT NULL ,
+ `customer_id` int NOT NULL ,
+ `user_id`     int NOT NULL ,
+ `created`     datetime NOT NULL ,
+ `name`        varchar(250) NOT NULL ,
+ `type`        varchar(250) NOT NULL ,
+ `description` mediumtext NULL ,
+
+PRIMARY KEY (`album_id`),
+KEY `fkIdx_147` (`customer_id`),
+CONSTRAINT `FK_147` FOREIGN KEY `fkIdx_147` (`customer_id`) REFERENCES `customers` (`customer_id`),
+KEY `fkIdx_150` (`user_id`),
+CONSTRAINT `FK_150` FOREIGN KEY `fkIdx_150` (`user_id`) REFERENCES `users` (`user_id`)
+);
+
+
+
+
+
+
+-- ************************************** `playlists-directories`
+
+CREATE TABLE `playlists-directories`
+(
+ `album_id`     int NOT NULL ,
+ `directory_id` int NOT NULL ,
+ `user_id`      int NOT NULL ,
+ `created`      datetime NOT NULL ,
+ `order`        int NOT NULL ,
+
+PRIMARY KEY (`album_id`, `directory_id`),
+KEY `fkIdx_161` (`album_id`),
+CONSTRAINT `FK_161` FOREIGN KEY `fkIdx_161` (`album_id`) REFERENCES `playlists` (`album_id`),
+KEY `fkIdx_165` (`directory_id`),
+CONSTRAINT `FK_165` FOREIGN KEY `fkIdx_165` (`directory_id`) REFERENCES `directories` (`directory_id`),
+KEY `fkIdx_169` (`user_id`),
+CONSTRAINT `FK_169` FOREIGN KEY `fkIdx_169` (`user_id`) REFERENCES `users` (`user_id`)
+);
+
+
+
+
+
+-- ****************** SqlDBM: MySQL ******************;
+-- ***************************************************;
+
+
+-- ************************************** `schools`
+
+CREATE TABLE `schools`
+(
+ `school_id`  int NOT NULL ,
+ `name_short` varchar(250) NOT NULL ,
+ `name_long`  varchar(250) NOT NULL ,
+
+PRIMARY KEY (`school_id`)
+);
+
+
+
+
+
+
+-- ************************************** `playlists_media`
+
+CREATE TABLE `playlists_media`
+(
+ `album_id` int NOT NULL ,
+ `media_id` int NOT NULL ,
+ `user_id`  int NOT NULL ,
+ `created`  datetime NOT NULL ,
+ `order`    int NOT NULL ,
+
+PRIMARY KEY (`album_id`, `media_id`),
+KEY `fkIdx_198` (`album_id`),
+CONSTRAINT `FK_198` FOREIGN KEY `fkIdx_198` (`album_id`) REFERENCES `playlists` (`album_id`),
+KEY `fkIdx_202` (`media_id`),
+CONSTRAINT `FK_202` FOREIGN KEY `fkIdx_202` (`media_id`) REFERENCES `media` (`media_id`),
+KEY `fkIdx_205` (`user_id`),
+CONSTRAINT `FK_205` FOREIGN KEY `fkIdx_205` (`user_id`) REFERENCES `users` (`user_id`)
 );
 
 
@@ -368,35 +377,19 @@ CONSTRAINT `FK_143` FOREIGN KEY `fkIdx_143` (`user_id`) REFERENCES `users` (`use
 
 CREATE TABLE `users`
 (
- `user_id`                int NOT NULL ,
- `name_first`             varchar(100) NOT NULL ,
- `name_last`              varchar(100) NOT NULL ,
- `username`               varchar(100) NOT NULL ,
- `password`               varchar(100) NOT NULL ,
- `gt_id`                  varchar(100) NULL ,
- `avatar`                 varchar(100) NULL ,
- `contact_phone_areacode` int NULL ,
- `contact_phone_number`   int NULL ,
- `contact_email`          varchar(100) NULL ,
- `created`                datetime NOT NULL ,
+ `user_id`              int NOT NULL ,
+ `created`              datetime NOT NULL ,
+ `name_first`           varchar(100) NOT NULL ,
+ `name_last`            varchar(100) NOT NULL ,
+ `username`             varchar(100) NOT NULL ,
+ `password`             varchar(100) NOT NULL ,
+ `gt_id`                varchar(100) NOT NULL ,
+ `avatar`               varchar(100) NULL ,
+ `contact_phone_code`   int NULL ,
+ `contact_phone_number` int NULL ,
+ `contact_email`        varchar(100) NULL ,
 
 PRIMARY KEY (`user_id`)
-);
-
-
-
-
-
-
--- ************************************** `schools`
-
-CREATE TABLE `schools`
-(
- `school_id`  int NOT NULL ,
- `name_short` varchar(250) NOT NULL ,
- `name_long`  varchar(250) NOT NULL ,
-
-PRIMARY KEY (`school_id`)
 );
 
 
