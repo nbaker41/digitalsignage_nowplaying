0c71b99c0e5956b0c24b979cbe37110bf2bf1b9b
Dunston <cdunston3@gatech.edu>
2020-02-26 12:41:27 -0500

    playlists is now an ng-switch.

--- components/main/playlists/playlists.js
+++ components/main/playlists/playlists.js
@@ -1,6 +1,6 @@
-(function(){
+(function () {
 
-     let playlists = angular.module("playlists", []);
+	let playlists = angular.module("playlists", []);
 
 	playlists.component("playlists", {
 		controller: "playlistsCtrl",
@@ -8,14 +8,23 @@
 		templateUrl: "components/main/playlists/playlists.html"
 	});
 
-     playlists.controller(
-     "playlistsCtrl", function($scope, $rootScope){
-     this.app = $scope.$parent.app;
-     let playlists = this;
+	playlists.controller(
+	"playlistsCtrl",
+	function ($scope, $rootScope) {
+		this.app = $scope.$parent.app;
+		let playlists = this;
 
-          playlists.app.data = $rootScope.data;
-          console.log(playlists.app.data);
-          
-     });
+		playlists.app.data = $rootScope.data;
+		console.log(playlists.app.data);
+
+	// ng-switch on  #playlists-container
+		playlists.switchObject = "media";
+
+	// switch  playlists
+		playlists.switchPlaylists = function(switchObject){
+			playlists.switchObject = "directory";
+		}
+
+	});
 
 })();
