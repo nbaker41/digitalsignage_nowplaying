4f8cd58e5588b44ec7041fc7afbe4e82f924cc98
Dunston <cdunston3@gatech.edu>
2020-03-02 14:57:42 -0500

    header/menu for devices on nowplaying

--- components/header/header.js
+++ components/header/header.js
@@ -1,7 +1,6 @@
-(function(){
+(function () {
 
-     let header = angular.module("header", [
-     ]);
+	let header = angular.module("header", []);
 
 	header.component("header", {
 		controller: "headerCtrl",
@@ -9,13 +8,35 @@
 		templateUrl: "components/header/header.html"
 	});
 
-     header.controller(
-     "headerCtrl", function($scope){
-     this.app = $scope.$parent.app;
-     let header = this;
+	header.controller(
+	"headerCtrl",
+	function ($scope) {
+	this.app = $scope.$parent.app;
+	let header = this;
 
-          
+		header.lines = {
+			number: 98,
+			get: function(x){
+				return new Array(x);
+			},
+			set: function(x){
+				this.number = x;
+			}
+		}
 
-     });
+	// menu toggle
+		header.menu = {
+			isOpen: false,
+			toggle: function(){
+				if(this.isOpen == true){
+					this.isOpen = false;
+				} else{
+					this.isOpen = true;
+				}
+			}
+			
+		}
+
+	});
 
 })();
